7c777eb227eb561726bfd1ee2a85461585b85e14
Roberto <robhhr@outlook.com>
2019-07-24 19:36:57 -0700

    add custom fonts to workspace

--- themes/quotes/home.php
+++ themes/quotes/home.php
@@ -1,9 +1,5 @@
 <?php echo "HELLO, WORLD" ?>
 
-<div class="logo">	
-	<?php echo '<style type="text/css"> .logo { background-image:url('.get_stylesheet_directory_uri().'/images/qod-logo.svg) !important; height: 30px !important; width: 45px !important; background-size: 100% !important; background-repeat: no-repeat;}</style>';?>		
-</div>
-
 <?php
 /**
  * The template for displaying all pages.
