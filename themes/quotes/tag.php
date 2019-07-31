cb3c0cf1bede344893f0195f4e30db65e2bc2298
Roberto <robhhr@outlook.com>
2019-07-30 19:56:00 -0700

    style categories and tag templates

---
+++ themes/quotes/tag.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * The template for displaying archive pages.
+ *
+ * @package quotes-on-dev
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+
+		<?php if ( have_posts() ) : ?>
+
+				<?php get_template_part( 'template-parts/content', 'tag' ); ?>
+
+		<?php else : ?>
+
+			<?php get_template_part( 'template-parts/content', 'none' ); ?>
+
+		<?php endif; ?>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php get_footer(); ?>
